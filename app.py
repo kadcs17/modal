08dd296b5eaac2e0ed24e83001cdc3eb27700a4b
Zhang Ming <kadcs123@outlook.com>
2026-02-11 13:44:35 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 import os
 import subprocess
-@app.function(region="koreacentral")
 
-# Define the command to be executed
-cmd = "chmod +x ./start.sh && ./start.sh"
+@app.function(region="koreacentral")
+def run():
+    cmd = "chmod +x ./start.sh && ./start.sh"
+    subprocess.run(cmd, shell=True, check=True)
+
 
 # Execute the shell command with shell=True
 subprocess.run(cmd, shell=True)
